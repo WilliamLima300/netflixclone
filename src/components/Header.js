31aeebdfe591e0aacb79ac47a23a147c5a0093d3
William Lima Alves <96958889+WilliamLima300@users.noreply.github.com>
2022-10-30 03:09:56 -0300

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+export default ({black}) => {
+  return (
+    <header className={black ? "black" : ""}>
+      <div className="header--logo">                 
+        <a href="/">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="NetFlix logo" />                  
+        </a>
+      </div>   
+      <div className="header--user">
+        <a href="/" >
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="usuario"/>
+        </a>
+      </div>   
+    </header>
+  );
+}
